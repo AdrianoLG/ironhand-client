fb9b5a28bf4f114a0881e49a30af0d0db54029a2
Adriano Lorenzo Gonzalez <adri@adri.info>
2019-09-24 12:45:44 +0200

    Remove angular material fileupload module

--- src/app/components/culture/culture.module.ts
+++ src/app/components/culture/culture.module.ts
@@ -8,24 +8,22 @@ import { BookCreateComponent } from './book-create/book-create.component';
 import { CollectionFilterPipe } from '../../filters/collection-filter.pipe';
 import { BookDetailComponent } from './book-detail/book-detail.component';
 import { BookUpdateComponent } from './book-update/book-update.component';
-import { MatFileUploadModule } from 'angular-material-fileupload';
 
 @NgModule({
-	declarations: [
-		CultureComponent,
-		BookCreateComponent,
-		CollectionFilterPipe,
-		BookDetailComponent,
-		BookUpdateComponent
-	],
-	imports: [
-		CommonModule,
-		MaterialModule,
-		FormsModule,
-		ReactiveFormsModule,
-		CultureRoutingModule,
-		MatFileUploadModule
-	],
-	exports: [ CollectionFilterPipe ]
+  declarations: [
+    CultureComponent,
+    BookCreateComponent,
+    CollectionFilterPipe,
+    BookDetailComponent,
+    BookUpdateComponent
+  ],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CultureRoutingModule,
+  ],
+  exports: [ CollectionFilterPipe ]
 })
 export class CultureModule {}
