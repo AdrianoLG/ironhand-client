b8bf82f45f4364de02239719bdb48ac83387619e
Adriano Lorenzo González <adri@adri.info>
2020-03-04 21:01:50 +0100

    Filter pipe title or name

--- src/app/filters/collection-filter.pipe.ts
+++ src/app/filters/collection-filter.pipe.ts
@@ -4,9 +4,17 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'collectionFilter'
 })
 export class CollectionFilterPipe implements PipeTransform {
-  transform(value: any, arg: any): any {
+  transform(value: any, arg: any, field: any): any {
+    console.log(value);
+    console.log(arg);
+    console.log(field);
     if (value != null && arg != null) {
-      const filteredCollections = value.filter(collection => collection.title.search(new RegExp(arg, 'i')) >= 0);
+      let filteredCollections;
+      if (field === 'title') {
+        filteredCollections = value.filter(collection => collection.title.search(new RegExp(arg, 'i')) >= 0);
+      } else if (field === 'name') {
+        filteredCollections = value.filter(collection => collection.name.search(new RegExp(arg, 'i')) >= 0);
+      }
       if (filteredCollections !== 0) {
         return filteredCollections;
       }
